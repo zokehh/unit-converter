c5b6a9dc205531b2a2202165e7dc1d0442e41d10
zokehh <109703675+zokehh@users.noreply.github.com>
2022-09-22 13:10:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let liter = 0.264
 let kilogram = 2.204
 let convertEl = document.getElementById("conver-el")
 let inputEl = document.getElementById("input-el")
-let hello1 = document.querySelector(".siemano1")
-let hello2 = document.querySelector(".siemano2")
-let hello3 = document.querySelector(".siemano3")
+let meters = document.querySelector(".siemano1")
+let liters = document.querySelector(".siemano2")
+let kilos = document.querySelector(".siemano3")
 
 convertEl.addEventListener("click", function() {
         let base = inputEl.value
-        hello1.textContent = `${base} meters = ${base * meter.toFixed(3)} feets | ${base * meter.toFixed(3)} feets = ${base} meter`
-        hello2.textContent = `${base} liters = ${base * liter.toFixed(3)} gallons | ${base * liter.toFixed(3)} gallons = ${base} liters`
-        hello3.textContent = `${base} kilos = ${base * kilogram.toFixed(3)} pounds | ${base * kilogram.toFixed(3)} pounds = ${base} kilos`
+        meters.textContent = `${base} meters = ${base * meter.toFixed(3)} feets | ${base * meter.toFixed(3)} feets = ${base} meter`
+        liters.textContent = `${base} liters = ${base * liter.toFixed(3)} gallons | ${base * liter.toFixed(3)} gallons = ${base} liters`
+        kilos.textContent = `${base} kilos = ${base * kilogram.toFixed(3)} pounds | ${base * kilogram.toFixed(3)} pounds = ${base} kilos`
 })
